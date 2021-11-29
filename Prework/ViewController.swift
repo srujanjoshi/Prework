208453b6351c7cce821844bfebe0e8291878adf8
Srujan Joshi <srujanjoshi7@gmail.com>
2021-11-28 19:14:52 -0500

    Added Basic Functionality

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get the bill amount from the text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        //Get the total tip = bill*tipPercentage
+        let tipPercentages = [0.15,0.18,0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        //Update Total Amount
+        totalLabel.text = String(format:"$%.2f", total)
+        
+    }
+    
 }
 
